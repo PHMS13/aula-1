3440fb4e477781930536bdc4389f552750dc965b
PHMS13 <phms.13@hotmail.com>
2022-08-16 20:36:35 -0300

    100%

---
+++ Week - 1/day 3/index.js
@@ -0,0 +1,17 @@
+console.log("hello world!")
+
+function addNumbers() {
+    console.log("hello world!")
+}
+
+if (true) {
+
+}
+
+const addWords = () => {
+    console.log("hello world!")
+};
+
+addNumbers ();
+addWords();
+
